4a16b10324f46971cfee6daacfddaf2aa00a4150
Naimesh Choksi <netnbc@gmail.com>
2017-01-23 10:44:27 -0800

    Adding stringlab1_01232017.py

---
+++ stringlab1_01232017.py
@@ -0,0 +1,12 @@
+name1 = "Naimesh Choksi"
+name2 = "Abby Normal"
+name3 = "Al Coholic"
+name4 = raw_input("Enter fourth name: ")
+
+print
+print "{:>30}".format(name1)
+print "{:>30}".format(name2)
+print "{:>30}".format(name3)
+print "{:>30}".format(name4)
+print
+
