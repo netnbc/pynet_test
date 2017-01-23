7454bd2ddd36d855d8171704f5596c1650352d4c
Naimesh Choksi <netnbc@gmail.com>
2017-01-23 10:49:49 -0800

    Adding stringlab2_01232017.py

---
+++ stringlab2_01232017.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+ip_address = raw_input("Please enter IP address: ")
+ip_address = ip_addr.split(".")
+
+print
+print "{:<12} {:<12} {:<12} {:<12}".format(*ip_address)
+print
+
