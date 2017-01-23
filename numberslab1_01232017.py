e7301455584ba959e69e62c1e81dc617bdfb639c
Naimesh Choksi <netnbc@gmail.com>
2017-01-23 10:59:55 -0800

    Adding numberslab1_01232017.py

---
+++ numberslab1_01232017.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+number1 = int(raw_input("Enter first number: "))
+number2 = int(raw_input("Enter second number: "))
+
+print "Sum: {}".format(number1 + number2)
+print "Difference: {}".format(number1 - number2)
+print "Product: {}".format(number1 * number2)
+print "Division: {:.2f}".format(number1/float(number2))
+
+print
