da771779d48021000ba3454b8691b4d19ea286ad
Naimesh Choksi <netnbc@gmail.com>
2017-01-23 10:53:22 -0800

    Adding stringlab2_01232017.py ... yes again ...

--- stringlab2_01232017.py
+++ stringlab2_01232017.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 ip_address = raw_input("Please enter IP address: ")
-ip_address = ip_addr.split(".")
+ip_address = ip_address.split(".")
 
 print
 print "{:<12} {:<12} {:<12} {:<12}".format(*ip_address)
